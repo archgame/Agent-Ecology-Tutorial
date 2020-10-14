375b5b932abf4c84d4f703c5a486e8f5786cf6eb
archgame <matthewjosephconway@gmail.com>
2020-10-13 23:02:17 -0700

    Tutorial 004: Conveyance, Dictionary<>, FindPath, NextDestination, CalculatePath()

---
+++ Assets/Scripts/Conveyance.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyance : MonoBehaviour
+{
+    public GameObject[] Path;
+    public float Weight = 3.0f;
+    public float Speed = 4.0f;
+
+    private Destination _destination;
+    private Dictionary<Guest, Vector3> _guests = new Dictionary<Guest, Vector3>();
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _destination = GetComponentInChildren<Destination>();
+    }
+
+    // Update is called once per frame
+    public void ConveyanceUpdate(Guest guest)
+    {
+        if (!_guests.ContainsKey(guest)) //add guest to dictionary
+        {
+            _guests.Add(guest, Path[1].transform.position);
+        }
+
+        guest.transform.position = Vector3.MoveTowards(
+            guest.transform.position,
+            _guests[guest],
+            Time.deltaTime * Speed
+            );
+
+        if (Vector3.Distance(guest.transform.position, _guests[guest]) < 0.01)
+        {
+            _guests.Remove(guest);
+            guest.NextDestination();
+        }
+    }
+
+    public float WeightedTravelDistance()
+    {
+        float distance = 0;
+        //guard statement
+        if (Path.Length < 2) return distance;
+
+        //getting the total path distance
+        for (int i = 1; i < Path.Length; i++)
+        {
+            GameObject go1 = Path[i - 1];
+            GameObject go2 = Path[i];
+
+            float d = Vector3.Distance(go1.transform.position, go2.transform.position);
+            distance += d;
+        }
+
+        //we scale the distance by the weight factor
+        distance /= Weight; //distance = distance/Weight;
+
+        return distance;
+    }
+
+    public Vector3 StartPosition()
+    {
+        if (Path.Length == 0) { return Vector3.zero; }
+        return Path[0].transform.position;
+    }
+
+    public Vector3 EndPosition()
+    {
+        if (Path.Length == 0) { return Vector3.zero; }
+        return Path[Path.Length - 1].transform.position;
+        //GameObject go = Path[Path.Length - 1];
+        //Transform t = go.transform;
+        //Vector3 position = t.position;
+        //return position;
+        //doesn't go to this line
+    }
+
+    public Destination GetDestination()
+    {
+        return _destination;
+    }
+}
--- Assets/Scripts/Guest.cs
+++ Assets/Scripts/Guest.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class Guest : MonoBehaviour
 {
-    public enum Action { BATHING, WALKING, FOLLOWING }
+    public enum Action { BATHING, WALKING, FOLLOWING, RIDING }
 
     //public global variables
     public Destination Destination; //where the agent is going
@@ -17,6 +17,8 @@ public class Guest : MonoBehaviour
     private float _bathTime = 0; //how long the agent has been in the bath
 
     private NavMeshAgent _agent; //our Nav Mesh Agent Component
+    private Conveyance _currentConveyance = null;
+    private List<Destination> _destinations = new List<Destination>();
 
     /// <summary>
     /// Called only once right after hitting Play
@@ -26,23 +28,30 @@ public class Guest : MonoBehaviour
         _agent = GetComponent<NavMeshAgent>();
         Status = Action.WALKING;
         UpdateDestination();
+        FindPath();
     }
 
     // Update is called once per frame
     public void GuestUpdate()
     {
+        if (Status == Action.RIDING)
+        {
+            _currentConveyance.ConveyanceUpdate(this);
+        }
         if (Status == Action.BATHING)
         {
             _bathTime += Time.deltaTime; //_bathTime = _bathTime + Time.deltaTime
             if (_bathTime > BathTime)
             {
                 Status = Action.WALKING;
-                _bathTime = 0; //++++
-                Destination.RemoveGuest(this); //++++
+                _bathTime = 0;
+                Destination.RemoveGuest(this);
+                _destinations.RemoveAt(0);
 
                 GameObject entrance = GameObject.Find("Entrance");
                 Destination = entrance.GetComponent<Destination>();
                 UpdateDestination();
+                FindPath();
             }
             //++++
             return; //so it doesn't run any code below
@@ -58,7 +67,14 @@ public class Guest : MonoBehaviour
         //test agent distance from destination
         if (Vector3.Distance(transform.position, Destination.transform.position) < 1.1f)
         {
-            if (Destination.tag == "Bath")
+            if (Destination.GetComponentInParent<Conveyance>())
+            {
+                Status = Action.RIDING;
+                _agent.enabled = false;
+                _currentConveyance = Destination.GetComponentInParent<Conveyance>();
+                return;
+            }
+            else if (Destination.tag == "Bath")
             {
                 StartBath();
                 return;
@@ -80,6 +96,65 @@ public class Guest : MonoBehaviour
     {
         _agent.SetDestination(Destination.transform.position);
         _agent.isStopped = false;
+    }
+
+    public void NextDestination()
+    {
+        _agent.enabled = true;
+        _destinations.RemoveAt(0);
+        Destination = _destinations[0];
+        Status = Action.WALKING;
+        FindPath(); //this allows multiple conveyances
+    }
+
+    public void FindPath()
+    {
+        NavMeshPath navMeshPath = _agent.path;
+        if (!_agent.CalculatePath(Destination.transform.position, navMeshPath)) return;
+
+        Vector3[] path = navMeshPath.corners;
+        if (path.Length < 2) return;
+
+        //get walking path distance
+        float distance = 0;
+        for (int i = 1; i < path.Length; i++)
+        {
+            distance += Vector3.Distance(path[i - 1], path[i]);
+            Debug.DrawLine(path[i - 1], path[i], Color.red);
+        }
+        Debug.Break();
+        //code below break still runs
+
+        //test all conveyances
+        _currentConveyance = null;
+        Vector3 guestPosition = transform.position;
+        Vector3 destinationPosition = Destination.transform.position;
+        Conveyance[] conveyances = GameObject.FindObjectsOfType<Conveyance>();
+        foreach (Conveyance c in conveyances)
+        {
+            float distToC = Vector3.Distance(guestPosition, c.StartPosition());
+            float distC = c.WeightedTravelDistance();
+            float distFromC = Vector3.Distance(c.EndPosition(), destinationPosition);
+
+            Debug.DrawLine(guestPosition, c.StartPosition(), Color.cyan);
+            Debug.DrawLine(c.StartPosition(), c.EndPosition(), Color.cyan);
+            Debug.DrawLine(c.EndPosition(), destinationPosition, Color.cyan);
+
+            if (distance > distToC + distC + distFromC)
+            {
+                _currentConveyance = c;
+                distance = distToC + distC + distFromC;
+            }
+        }
+
+        if (_currentConveyance == null) { UpdateDestination(); return; }
+
+        //update destinations
+        _destinations.Clear();
+        _destinations.Add(_currentConveyance.GetDestination());
+        _destinations.Add(Destination);
+        Destination = _destinations[0];
+        UpdateDestination();
     }
 
     /// <summary>
